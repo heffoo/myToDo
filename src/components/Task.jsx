664e3965e648c855d3e618d430ea2359bc23434b
heffoo <57847291+heffoo@users.noreply.github.com>
2020-10-22 13:53:39 +0300

    213

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,35 +1,40 @@
 import React, { useState } from "react";
-import createIcon from '../img/create.svg'
-import saveIcon from '../img/save.svg'
-
+import createIcon from "../img/create.svg";
+import saveIcon from "../img/save.svg";
 
 import "../App.css";
 
-const Task = ({ task, index, delTask,  checkedTask, editTask }) => {
-    const [edited, setEdited] = useState(false);
+const Task = ({ task, index, delTask, checkedTask, editTask }) => {
+  const [edited, setEdited] = useState(false);
 
-    const editFunc = () => {
-       if (edited) {
+  const editFunc = () => {
+    if (edited) {
       editTask(index, document.getElementsByClassName("editTaskInp")[0].value);
-      
     }
     setEdited(!edited);
-  }
+  };
   return (
     <>
       <li className="mainLi">
         <input type="checkbox" className="checkbox" checked={task.completed} onChange={() => checkedTask(task.id)} />
-        {edited ? <div className="forInput"><input type="text" className="editTaskInp" defaultValue={task.title} onKeyPress={(e) => e.key === "Enter" && editFunc() }/> </div> : <div className={task.completed ? "completed" : "simpleLi"}>{task.title}</div>}
+        {edited ? (
+          <div className="forInput">
+            <input
+              type="text"
+              className="editTaskInp"
+              defaultValue={task.title}
+              onKeyPress={(e) => e.key === "Enter" && editFunc()}
+            />
+          </div>
+        ) : (
+          <div className={task.completed ? "completed" : "simpleLi"}>{task.title}</div>
+        )}
         {/* <span>{task.date}</span> */}
-       
-        <button
-          className="editTask"
-          onClick={editFunc}
-          
-        >
-          {edited ? <img src={saveIcon}/> : <img src={createIcon}/>}
+
+        <button className="editTask" onClick={editFunc}>
+          {edited ? <img src={saveIcon} /> : <img src={createIcon} />}
         </button>
-         <button className="delTask" onClick={() => delTask(task.id)}>
+        <button className="delTask" onClick={() => delTask(task.id)}>
           ✕
         </button>
       </li>
