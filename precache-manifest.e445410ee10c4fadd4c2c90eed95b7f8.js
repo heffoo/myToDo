ef945ee1d4c31a3b196dd8adb1813bbafee55e1b
heffoo <57847291+heffoo@users.noreply.github.com>
2020-10-22 14:07:46 +0300

    Updates

---
+++ precache-manifest.e445410ee10c4fadd4c2c90eed95b7f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4485dbabc35255be36a73219740f74",
+    "url": "/myToDo/index.html"
+  },
+  {
+    "revision": "bdc8bc096084242d2bf4",
+    "url": "/myToDo/static/css/main.38667ae4.chunk.css"
+  },
+  {
+    "revision": "11d908ed619f65f6dc69",
+    "url": "/myToDo/static/js/2.972e1ec3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myToDo/static/js/2.972e1ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdc8bc096084242d2bf4",
+    "url": "/myToDo/static/js/main.3f4f1eeb.chunk.js"
+  },
+  {
+    "revision": "97568d620c7e097f6561",
+    "url": "/myToDo/static/js/runtime-main.922588a4.js"
+  },
+  {
+    "revision": "539b3c382ff24fe86b17f31735fb8e74",
+    "url": "/myToDo/static/media/create.539b3c38.svg"
+  },
+  {
+    "revision": "a85d4bf5700e9e31e6f90ea905805306",
+    "url": "/myToDo/static/media/save.a85d4bf5.svg"
+  }
+]);
